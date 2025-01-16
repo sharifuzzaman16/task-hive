fe38403232c8342622f43aeeff05db597aaacf65
sharifuzzaman24 <private.sharifuzzaman@gmail.com>
2025-01-16 14:02:30 +0600

    featched my-task and displayed it in a table format

--- src/pages/Dashboard/Buyer/MyTasks/MyTasks.jsx
+++ src/pages/Dashboard/Buyer/MyTasks/MyTasks.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import React, { useContext } from "react";
+import { FaRegEdit } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa6";
+import useAxiosPublic from "../../../../hooks/useAxiosPublic";
+import { AuthContext } from "../../../../context/AuthProvider";
 
 const MyTasks = () => {
-  const tasks = [
-    { id: 1, name: "Logo Design", status: "In Progress" },
-    { id: 2, name: "Website Development", status: "Completed" },
-  ];
+
+  const axiosPublic = useAxiosPublic();
+  const {user} = useContext(AuthContext);
+  const userEmail = user?.email;
+  
+  const {refetch, data: myTasks = []} = useQuery({
+    queryKey: ['myTasks', userEmail],
+    queryFn: async () => {
+      const res = await axiosPublic.get(`/my-tasks?email=${userEmail}`);
+      return res.data;
+    },
+    enabled: !!userEmail
+  }) 
+  console.log(myTasks);
 
   return (
     <div>
@@ -14,14 +29,19 @@ const MyTasks = () => {
           <thead className="bg-gray-100">
             <tr>
               <th className="text-left px-4 py-2">Task</th>
-              <th className="text-left px-4 py-2">Status</th>
+              <th className="text-left px-4 py-2">Completion Date</th>
+              <th className="text-left px-4 py-2">Action</th>
             </tr>
           </thead>
           <tbody>
-            {tasks.map((task) => (
-              <tr key={task.id} className="border-t">
-                <td className="px-4 py-2">{task.name}</td>
-                <td className="px-4 py-2">{task.status}</td>
+            {myTasks.map((task) => (
+              <tr key={task._id} className="border-t">
+                <td className="px-4 py-2">{task.task_title}</td>
+                <td className="px-4 py-2">{task.completion_date}</td>
+                <td className="px-4 py-2 flex gap-2 items-center">
+                    <FaRegEdit className="text-2xl text-success-green"></FaRegEdit>
+                    <FaTrash className="text-2xl text-error-red"></FaTrash>
+                </td>
               </tr>
             ))}
           </tbody>
